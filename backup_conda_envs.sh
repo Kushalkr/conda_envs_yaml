ded31bdc2b1d1023e2d28b66797950d0c11c211d
Kushal Keshavamurthy Raviprakash <Kushalkr@users.noreply.github.com>
2025-03-30 17:48:13 +0530

    30-03-2025

--- backup_conda_envs.sh
+++ backup_conda_envs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-for ienv in $(mamba env list | tail -n +3 | cut -d ' ' -f3); do mamba env export -n ${ienv} --no-builds | grep -v "^prefix" >conda_envs_yaml/${ienv}.yaml; done
+for ienv in $(mamba env list | tail -n +3 | cut -d ' ' -f3); do
+  echo "Backing up environment: ${ienv}"
+  mamba env export -n ${ienv} --no-builds | grep -v "^prefix" >conda_envs_yaml/${ienv}.yaml
+done
